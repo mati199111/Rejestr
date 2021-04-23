db65b31cecb83d8199fa26d6a7c45a9e838debdc
mati199111 <mati199111@gmail.com>
2021-04-23 20:07:06 +0200

    23042021

---
+++ Rejestr/Controllers/PeopleController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Rejestr.Models;
+
+namespace Rejestr.Controllers
+{
+    public class PeopleController : Controller
+    {
+        private OsobyEntities db = new OsobyEntities();
+
+        // GET: People
+        public ActionResult Index()
+        {
+            return View(db.People.ToList());
+        }
+
+        // GET: People/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            People people = db.People.Find(id);
+            if (people == null)
+            {
+                return HttpNotFound();
+            }
+            return View(people);
+        }
+
+        // GET: People/Create
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: People/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdPeople,Name,Lastname,Date_Birth,Country,City")] People people)
+        {
+            if (ModelState.IsValid)
+            {
+                db.People.Add(people);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(people);
+        }
+
+        // GET: People/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            People people = db.People.Find(id);
+            if (people == null)
+            {
+                return HttpNotFound();
+            }
+            return View(people);
+        }
+
+        // POST: People/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdPeople,Name,Lastname,Date_Birth,Country,City")] People people)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(people).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(people);
+        }
+
+        // GET: People/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            People people = db.People.Find(id);
+            if (people == null)
+            {
+                return HttpNotFound();
+            }
+            return View(people);
+        }
+
+        // POST: People/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            People people = db.People.Find(id);
+            db.People.Remove(people);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
